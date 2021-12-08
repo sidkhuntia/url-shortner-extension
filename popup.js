e5953de4d28bd86d9f213c18e8a38b14f3b4c366
sidkhuntia <siddharthakhuntia@gmail.com>
2021-12-08 12:37:07 +0530

    hessss

--- popup.js
+++ popup.js
@@ -2,6 +2,11 @@ var button = document.querySelector(".short");
 var input = document.querySelector(".inputurl");
 var output = document.querySelector(".outputurl");
 var curenttaburl = "";
+const url = new URL("https://t.ly/api/v1/link/shorten");
+let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
 
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   console.log(tabs[0].url);
@@ -9,31 +14,29 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   input.value = tabs[0].url;
 });
 
-function(curenttaburl) {
-    chrome.storage.local.get({
-        bitlyApiKey: false,
-    }, function (res) {
-        let apiKey =  res.bitlyApiKey;
-        if(apiKey){
-        let longurl = encodeURIComponent(url);
-        var req = new XMLHttpRequest();
-        req.open("GET", "https://api-ssl.bitly.com/v3/shorten?access_token="+ apiKey + "&longUrl="+longurl+"&domain=bit.ly&", true);
-        req.addEventListener("load", function (e) {
-            //var resp = JSON.parse(req.responseText).shorturl.replace("http://", "https://");
-            if(JSON.parse(req.responseText).status_txt === "INVALID_ARG_ACCESS_TOKEN")
-            {
-                return 0;
-            }
-            var surl = (JSON.parse(req.responseText)).data.url;
-            saveToStorage(url, surl);
-            copyTextToClipboard(surl);
-        }, false);
-        req.addEventListener("error", function (e) {
-            //var resp = JSON.parse(req.responseText).shorturl.replace("http://", "https://");
-           console.log("errro");
-        }, false);
-        req.send();
-    }
-    });
+
+
+let body = {
+    "long_url": JSON.stringify(curenttaburl),
+    "domain": "https://t.ly/",
+    "api_token": "sYLBhJWitAKosyEUM6lFfcjmebLqnbo3jMwuyBgZnFg4PSSxb0YCQaA8QAlS",
 }
+
+fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(body)
+})
+    .then(response => response.json())
+    .then(json => {
+        console.log(json)
+        output.value = json.short_url;   
+    });
+
+
+
+
+
+
 //    b1b9168a05dd400d143c4637b310804ab45d5b31
+//    sYLBhJWitAKosyEUM6lFfcjmebLqnbo3jMwuyBgZnFg4PSSxb0YCQaA8QAlS
